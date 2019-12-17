e1e9fa60f44b352758628d6ffe383a6d39b7a80f
Bruno Costa <bruno.ncosta9@gmail.com>
2019-12-17 11:54:10 +0000

    Update config.php

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ return [
          "current"  => "http://api.openweathermap.org/data/2.5/weather",
          "forecast" => "http://api.openweathermap.org/data/2.5/forecast"
          ],
-         "key" => "4cb7aa50543720e48ac590abebd3429d"
+         "key" => "YOUT_KEY"
       ]
    ];
